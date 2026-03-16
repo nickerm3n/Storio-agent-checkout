93a5889204534bff0b2d0bb28993f95f83c139d9
Mykyta Kurylko <mykyta_kurylko@epam.com>
2026-03-16 15:14:03 +0100

    Update Vite configuration to set base path for deployment
    - Added a base path of "/Storio-agent-checkout/" in `vite.config.ts` to ensure correct routing for the deployed application.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
+  base: "/Storio-agent-checkout/",
   plugins: [react()],
   server: {
     port: 5174
